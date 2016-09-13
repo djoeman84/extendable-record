1fd2d913cae89db34abc0600283037a91a2a1173
Joe Delgado <djoeman84@gmail.com>
2016-09-13 04:02:49 -0400

    Adding min.js and sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
+/* eslint-disable */
 var path = require('path');
 var webpack = require('webpack');
+var PROD = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  devtool: 'source-map',
   entry: [
     './bin/bower_root.js',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: PROD ? 'bundle.min.js' : 'bundle.js',
     publicPath: '/',
   },
-  plugins: [],
+  plugins: PROD ? [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  ] : [],
   module: {
     loaders: [
       {
@@ -20,6 +27,6 @@ module.exports = {
           /node_modules/,
         ],
       },
-    ]
-  }
+    ],
+  },
 };
